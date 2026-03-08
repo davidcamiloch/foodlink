e442df74a25edaf045dc1b9807496bce4f85c156
davidcamiloch <davidcamiloch@ufps.edu.co>
2026-03-08 17:05:33 -0500

    segundo commit prueba

--- app/src/main/java/com/foodlink/app/model/Menu.java
+++ app/src/main/java/com/foodlink/app/model/Menu.java
@@ -1,4 +1,17 @@
 package com.foodlink.app.model;
 
+import com.foodlink.app.util.ColaP;
+
 public class Menu {
+    private ColaP<Plato> platos;
+
+    public Menu(){
+
+    }
+
+    public Menu(ColaP<Plato> platos){
+        this.platos = platos;
+    }
+
+
 }
--- app/src/main/java/com/foodlink/app/model/Restaurante.java
+++ app/src/main/java/com/foodlink/app/model/Restaurante.java
@@ -1,4 +1,28 @@
 package com.foodlink.app.model;
 
 public class Restaurante {
+
+
+    private String nombre;
+    private String direccion;
+    private String descripcion;
+    private String telefono;
+    private double ratingPromedio;
+
+
+    private Menu menu;
+
+
+    public Restaurante(){
+
+    }
+    public Restaurante(String nombre, String direccion, String descripcion, String telefono, double ratingPromedio, Menu menu){
+        this.nombre = nombre;
+        this. direccion = direccion;
+        this.descripcion = descripcion;
+        this.telefono = telefono;
+        this.ratingPromedio = ratingPromedio;
+        this.menu = menu;
+    }
+
 }
